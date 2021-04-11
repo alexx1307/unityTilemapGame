2c06680319d34454a424f3f98ed9a76c61c54e42
Lukasz <alexx1307@gmail.com>
2021-04-11 15:41:34 +0200

    initial branch

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+public class Player{
+    public string name;
+    private int res1;
+    private int res2;
+
+    public Player(string _name)
+    {
+        this.name = _name;
+        this.res1 = 10;
+
+      this.res2 = 10;  
+    }
+}
