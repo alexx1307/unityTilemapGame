a8006ea103de4f0457b2f5f815c7a317e1b45cb3
Lukasz <alexx1307@gmail.com>
2021-04-12 15:25:59 +0200

    renaming grid manager

--- Assets/Scripts/BuilderManager.cs
+++ Assets/Scripts/BuilderManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class GridManager : MonoBehaviour
+public class BuilderManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private Grid grid;
