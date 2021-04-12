2ee263ff0b60c422465477e1a3b392f3530f82b2
Tobel <tobiasz.salik@gmail.com>
2021-04-12 17:53:43 +0200

    'przyciski'

--- Assets/Scripts/BuilderManager.cs
+++ Assets/Scripts/BuilderManager.cs
@@ -2,27 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class BuilderManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private Grid grid;
     public Tilemap tilemap;
-    public TileBase tile;
+    public GameObject ButtonPrefab;
+    public Transform Panel;
+    
     void Start()
     {
         grid = GetComponent<Grid>();
+        createButton();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(worldPoint);
-            Vector3Int position = grid.WorldToCell(worldPoint);
-            Debug.Log(position);
-            tilemap.SetTile(position, tile);
+        // if(Input.GetMouseButtonDown(0)){
+        //     Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //     Debug.Log(worldPoint);
+        //     Vector3Int position = grid.WorldToCell(worldPoint);
+        //     Debug.Log(position);
+        //     tilemap.SetTile(position, tile);
+        // }
+    }
+
+    void createButton()
+    {
+        var buildings = GetComponentsInChildren<Building>();
+        foreach (var building in buildings)
+        {
+            var Button = Instantiate(ButtonPrefab);
+            Button.GetComponent<Image>().sprite = building.Sprite;
+            Button.transform.SetParent(Panel);
         }
+        
     }
 }
---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    public Sprite Sprite;
+
+    public TileBase Tile;
+
+    // public Resources buildingCost;
+    // public Resources turnResourceChange;
+
+
+}
