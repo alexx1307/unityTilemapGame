7c59e144ffb370ce9f6a98d766023b12d454a304
patryk03 <patrykpiecuch3@gmail.com>
2021-04-26 13:25:02 +0200

    nasze zmiany w czasie Pana nieobecności

--- Assets/Scripts/BuilderManager.cs
+++ Assets/Scripts/BuilderManager.cs
@@ -3,32 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
-
 public class BuilderManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Grid grid;
+    public Grid grid;
     public Tilemap tilemap;
     public GameObject ButtonPrefab;
     public Transform Panel;
+    private Building CurrentBuilding;
+    [SerializeField] private GameObject Preview;
     
     void Start()
     {
-        grid = GetComponent<Grid>();
         createButton();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if(Input.GetMouseButtonDown(0)){
-        //     Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //     Debug.Log(worldPoint);
-        //     Vector3Int position = grid.WorldToCell(worldPoint);
-        //     Debug.Log(position);
-        //     tilemap.SetTile(position, tile);
-        // }
+        if(CurrentBuilding != null)
+        {
+            Vector3 pos =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Preview.transform.position = grid.GetCellCenterWorld(Vector3Int.FloorToInt(pos));
+            Preview.GetComponent<SpriteRenderer>().sprite = CurrentBuilding.Sprite;
+        }
+
+        if(Input.GetMouseButtonDown(0)){
+            Preview.SetActive(true);
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(worldPoint);
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            Debug.Log(position);
+            Debug.Log(CurrentBuilding);
+            if (CurrentBuilding != null)
+            {
+                Preview.SetActive(false);
+                tilemap.SetTile(position, CurrentBuilding.Tile);
+                CurrentBuilding = null;
+            }
+        }
     }
 
     void createButton()
@@ -38,8 +51,14 @@ public class BuilderManager : MonoBehaviour
         {
             var Button = Instantiate(ButtonPrefab);
             Button.GetComponent<Image>().sprite = building.Sprite;
+            Button.GetComponent<Button>().onClick.AddListener(() => pickBuilding(building));
             Button.transform.SetParent(Panel);
         }
         
+    }
+
+    private void pickBuilding(Building building)
+    {
+        CurrentBuilding = building;
     }
 }
